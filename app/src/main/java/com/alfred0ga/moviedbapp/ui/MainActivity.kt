77f02245a7c3473b5b7d69b337efa42fb5e59857
Alfred0 GA <alfred007ga@gmail.com>
2020-12-03 12:52:36 -0600

    Navigation complete

---
+++ app/src/main/java/com/alfred0ga/moviedbapp/ui/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.alfred0ga.moviedbapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.*
+import com.alfred0ga.moviedbapp.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.movieFragment, R.id.mapFragment, R.id.galleryFragment),
+            container_main
+        )
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottom_nav.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/alfred0ga/moviedbapp/ui/fragments/MovieFragment.kt
@@ -0,0 +1,7 @@
+package com.alfred0ga.moviedbapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.alfred0ga.moviedbapp.R
+
+class MovieFragment : Fragment(R.layout.fragment_movie) {
+}
